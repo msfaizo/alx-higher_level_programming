abf73a1186f54ea9762c801ad28aef119f4db048
msfaizo <faizosinani@gmail.com>
2022-06-29 00:41:02 +0300

    Number 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print('0{:d}'.format(i), end=', ')
-    else:
-        print('{:d}'.format(i), end=', ')
+for i in range(99):
+    print('{:02d}'.format(i), end=', ')
+print(99)
